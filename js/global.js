3a2d7ce04733415077c2d9170dae9362442e3502
Svetlana <slipets_S@ukr.net>
2019-12-06 11:47:21 +0200

    change form section style

--- js/global.js
+++ js/global.js
@@ -7,5 +7,18 @@ jQuery(document).ready(function($) {
             $(".agenda-one-info").removeClass('show')
         }
     });
-    
+
+    $(".nav-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $('.nav-menu li a').on('click', function(){
+        $('#menu li.current_page_item').removeClass('current_page_item');
+        $(this).addClass('current_page_item');
+    });
+
+
 });
